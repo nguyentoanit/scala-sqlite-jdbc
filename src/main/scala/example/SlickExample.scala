9b18f754d8872f263a546480256e8c1c81ed9cf2
Toan Nguyen <nguyentoandev@gmail.com>
2019-01-28 11:44:14 +0700

    Map table defination and Case class

--- src/main/scala/example/SlickExample.scala
+++ src/main/scala/example/SlickExample.scala
@@ -1,21 +1,30 @@
 package example
 
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.driver.SQLiteDriver.api._
+import scala.util.{Failure, Success}
 
-class Movies(tag: Tag) extends Table[(String, String, String, String, String, String)](tag, "Movies") {
+case class Movie(imdbID: String, title: String, year: String, director: String, actors: String, plot: String)
+class Movies(tag: Tag) extends Table[Movie](tag, "Movies") {
   def imdbID = column[String]("imdbID", O.PrimaryKey)
   def title = column[String]("Title")
   def year = column[String]("Year")
   def director = column[String]("Director")
   def actors = column[String]("Actors")
   def plot = column[String]("Plot")
-  def * = (imdbID, title, year, director, actors, plot)
+  /*
+      http://slick.lightbend.com/doc/3.2.3/schemas.html
+      Every table requires a * method containing a default projection.
+      This describes what you get back when you return rows (in the form of a table row object) from a query.
+
+      It is possible to define a mapped table that uses a custom type for its * projection by adding a bi-directional mapping with the <> operator
+   */
+  def * = (imdbID, title, year, director, actors, plot) <> (Movie.tupled, Movie.unapply)
 }
 
 object SlickExample extends App {
 
+  // Alongside the Table row class you also need a TableQuery value which represents the actual database table
   val movies = TableQuery[Movies]
 
   val db = Database.forConfig("movies")
@@ -24,11 +33,14 @@ object SlickExample extends App {
       (movies.schema).create
     )
     val setupFuture = db.run(setup)
-    println("Done")
+    setupFuture.onComplete({
+      case Success(response) => println(response)
+      case Failure(e) => println(e.getMessage)
+    })
     val q1 = for(m <- movies) yield m.title
     db.stream(q1.result).foreach(println)
     val insert = DBIO.seq(
-      movies += ("tt1234567", "Foo", "1970", "Bar", "Meh, Hmm", "Hmmmmmmm")
+      movies += Movie("tt1234567", "Foo", "1970", "Bar", "Meh, Hmm", "Hmmmmmmm")
     )
     val insertFuture = db.run(insert)
     insertFuture.onComplete( _ => db.stream(q1.result).foreach(println))
